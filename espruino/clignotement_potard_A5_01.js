ed17e76dc7ad1f4edc9cabf9129f0c9350215b4e
mrbbp <eric@mrbbp.com>
2021-05-03 18:34:06 +0200

    ajout de commentaires

--- espruino/clignotement_potard_A5_01.js
+++ espruino/clignotement_potard_A5_01.js
@@ -1,3 +1,8 @@
+/*  lit la valeur du potentiometre (en A5) et en déduit une durée
+    change la durée du setIntervalen fonction
+    =============================================================
+    modifie la vitesse de clignotement de la LED verte du pico
+*/
 var etat = 1;
 function onInit() {
   var duree = setInterval(function() {
