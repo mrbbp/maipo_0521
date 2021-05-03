b3714f4ff7efb0a0f51b22883bdb9d11edee8090
mrbbp <eric@mrbbp.com>
2021-05-03 18:31:06 +0200

    corrections typos

--- espruino/maipo_matrice_potard_01.js
+++ espruino/maipo_matrice_potard_01.js
@@ -1,33 +1,16 @@
 // sur la matrice:
 /*
-  pour
-  B13 : CLOCK
-  B14 : LOAD  / CS
-  B15 : DIN
-
-  le MAX7219 nécessite 5V.
-  qd branché à l'usb il faut brancher le VCC de la matrice au VOUT du Pico
-
-  pour TM1640
-  CLK D5 -> B13
-  DIN D7 -> B15
-     3V3 -> 3V3
-      5V -> Vout
-     GND -> GND
-
-
-  A la reinitialisation de la position de la chaine, le buffer graphique ne s'initialise pas correctement
-  *v03: segmentation de la chaine de texte en bouts de 10 caractères (+ rapide et pas d'erreur de buffer)
-  *v08: reboot() à chaque fin de texte (resoud le pb de l'afficheur?)
-  */
-
-/*
-var g; // TM1640
+  TM1640  ->  Espruino
+  ------------------
+  CLK D5  ->  B13
+  DIN D7  ->  B15
+     3V3  ->  3V3
+      5V  ->  Vout
+     GND  ->  GND
 */
+
 // crée le buffer graphique 8x8 en 1 bit
 let g = Graphics.createArrayBuffer(8,8,1);
-// connecte en spi
-let disp;
 // pour la vitesse de déilement
 var duree;
 g.flip = function() { disp.raw(g.buffer); }; // To send to the display Max7219
@@ -38,8 +21,6 @@ let phrase = texte.substr(0,12);
 let sDerCar = phrase.substr(10,2);
 let cPhrase = 0;
 let compteur = 8;
-// let longT = 0;
-// const lengthT = texte.length;
 let longP = 0;
 let decalage = 0;
 let debut = false;
@@ -79,9 +60,9 @@ function sendData() {
   // reset le buffer, ajoute la font, ecrit la chaine de caractères
   g.clear(); // efface le buffer (TM1640)
   g.setFont4x6();
-  /* une fois font utilisée, on connait la longueur de la chaine*/
+  /* une fois la font utilisée, on connait la taille (en px) de la chaine*/
   longP = g.stringWidth(phrase);
-  // calcul le decalage (en pix) entre chaque bout de phrase
+  // calcul le decalage (en px) entre chaque bout de phrase
   decalage = g.stringWidth(sDerCar);
   g.drawString(phrase,compteur,0);
   compteur--; // pour deplacer le texte
@@ -89,23 +70,25 @@ function sendData() {
   g.setRotation(1,true); // TM1640
   // mise à jour du display
   g.flip();
-  /* lecture du potar en A5*/
+  /* lecture du potentiomètre en A5*/
   potar = analogRead(A5); // valeur entre 0 et 1
   speed = 10+(potar*100);
-  //console.log(speed);
+  //console.log(speed); // pour le débug
   changeInterval( duree, speed);
 }
 
 function onInit() {
+  // charge la font
   require("Font4x6").add(Graphics);
   // spi mosi=DIN, sck=CLOCK
   g = require("TM1640").connect({din: B15, clk: B13});
   // efface l'afficheur
   g.clear();
+  // met à jour l'afficheur
   g.flip();
   console.log("init programme affichage matrice v2.0 - mrbbp ©2021");
   console.log("mém. used: "+Math.round(process.memory().usage/process.memory().total*100)+"%");
-  //lance l'ecriture sur la matrice
+  //lance l'écriture sur la matrice
   duree = setInterval(sendData, 500);
 }
 
